d0138d8a4d12f7a8fcf0ed6d9719f7919884bcaf
nico-frozencoffee <nico.frozencoffee@gmail.com>
2021-03-21 10:42:13 -0700

    linting and editorconfig rules

---
+++ .eslintrc.js
@@ -0,0 +1,21 @@
+module.exports = {
+  root: true,
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: [
+    'eslint:recommended',
+    'plugin:react/recommended',
+    'plugin:react-hooks/recommended',
+    'plugin:eslint-comments/recommended',
+    'plugin:@typescript-eslint/recommended',
+  ],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    sourceType: 'module',
+  },
+};
--- packages/sandbox/src/index.tsx
+++ packages/sandbox/src/index.tsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import ReactDom from 'react-dom';
 
+const f = 5;
+
 function App() {
   return <h1>Hello World!</h1>;
 }
